1bee2472476c1a856e669892e53108fb7b4d13f6
Gabriel <gabriel.bechtlufft0@gmail.com>
2026-08-19 10:52:12 -0300

    Atualização de arquivos

---
+++ prisma/migrations/20260818222701_init/migration.sql
@@ -0,0 +1,21 @@
+-- AlterTable
+ALTER TABLE "Listagem" ADD COLUMN     "categoriaServico" TEXT,
+ADD COLUMN     "tipoServico" TEXT;
+
+-- CreateTable
+CREATE TABLE "PasswordResetToken" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "usado" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "PasswordResetToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PasswordResetToken_token_key" ON "PasswordResetToken"("token");
+
+-- AddForeignKey
+ALTER TABLE "PasswordResetToken" ADD CONSTRAINT "PasswordResetToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
